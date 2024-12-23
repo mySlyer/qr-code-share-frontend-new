a7004ca5c38455e89c903a6ad1d4906f5cf235c0
murongchui <m_slyer@163.com>
2024-12-23 19:01:20 +0800

    fix: 完善box

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,46 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-	timeout: 1000 * 60,
-	headers: {
-		'Content-Type': 'application/json',
-	},
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  timeout: 1000 * 60,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 export default axiosInstance;
 
-axiosInstance.interceptors.request.use(
-	config => {
-		const token = localStorage.getItem('token');
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-		console.log('Sending request:', config);
-		return config;
-	},
-	error => {
-		console.error('Request error:', error);
-		return Promise.reject(error);
-	}
-);
+// axiosInstance.interceptors.request.use(
+// 	config => {
+// 		const token = localStorage.getItem('token');
+// 		if (token) {
+// 			config.headers.Authorization = `Bearer ${token}`;
+// 		}
+// 		console.log('Sending request:', config);
+// 		return config;
+// 	},
+// 	error => {
+// 		console.error('Request error:', error);
+// 		return Promise.reject(error);
+// 	}
+// );
 
-axiosInstance.interceptors.response.use(
-	response => {
-		console.log('Received response:', response);
-		return response.data;
-	},
-	error => {
-		console.error('Response error:', error);
-		if (error.response) {
-			const status = error.response.status;
-			if (status === 401) {
-				// 处理未授权错误
-			} else if (status === 404) {
-				// 处理未找到资源错误
-			} else if (status === 500) {
-				// 处理服务器内部错误
-			}
-		}
-		return Promise.reject(error);
-	}
-);
+// axiosInstance.interceptors.response.use(
+// 	response => {
+// 		console.log('Received response:', response);
+// 		return response.data;
+// 	},
+// 	error => {
+// 		console.error('Response error:', error);
+// 		if (error.response) {
+// 			const status = error.response.status;
+// 			if (status === 401) {
+// 				// 处理未授权错误
+// 			} else if (status === 404) {
+// 				// 处理未找到资源错误
+// 			} else if (status === 500) {
+// 				// 处理服务器内部错误
+// 			}
+// 		}
+// 		return Promise.reject(error);
+// 	}
+// );
